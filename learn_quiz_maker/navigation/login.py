8e56ea05bdcd5764ff104b9275b2509fff7b82b7
Le Kang Wang <lekang.wang@yahoo.ca>
2021-11-08 16:43:36 -0500

    added login.py

---
+++ learn_quiz_maker/navigation/login.py
@@ -0,0 +1,9 @@
+
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# Initialize webdriver
+driver = create_driver()
+
+driver.get("https://learn.uwaterloo.ca/d2l/")
